e3573feb02f7d42482dddaa2a7509274423dbb0c
CLEMENT DENAMIEL <clement.denamiel@iut.local>
2024-10-17 16:02:08 +0200

    changement de type de projet de l'api

---
+++ commerzi-api/src/main/java/com/commerzi/commerziapi/CommerziApiApplication.java
@@ -0,0 +1,13 @@
+package com.commerzi.commerziapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerziApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerziApiApplication.class, args);
+	}
+
+}
---
+++ commerzi-api/src/test/java/com/commerzi/commerziapi/CommerziApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commerzi.commerziapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerziApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
