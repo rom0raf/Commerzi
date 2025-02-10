9729056ce7205bcc4c87529adfc691abe7884936
d-clem <85625458+cleeem@users.noreply.github.com>
2025-02-10 17:03:56 +0100

    Ajout de la distance reelle depuis une API

---
+++ commerzi-api/src/main/java/com/commerzi/commerziapi/model/maps/Intersection.java
@@ -0,0 +1,66 @@
+package com.commerzi.commerziapi.model.maps;
+
+import java.util.List;
+
+public class Intersection {
+
+    private List<Boolean> entry;
+    private List<Integer> bearings;
+    private List<Double> location;
+    private int in;
+    private int out;
+
+    public Intersection() {
+    }
+
+    public List<Boolean> getEntry() {
+        return entry;
+    }
+
+    public void setEntry(List<Boolean> entry) {
+        this.entry = entry;
+    }
+
+    public List<Integer> getBearings() {
+        return bearings;
+    }
+
+    public void setBearings(List<Integer> bearings) {
+        this.bearings = bearings;
+    }
+
+    public List<Double> getLocation() {
+        return location;
+    }
+
+    public void setLocation(List<Double> location) {
+        this.location = location;
+    }
+
+    public int getIn() {
+        return in;
+    }
+
+    public void setIn(int in) {
+        this.in = in;
+    }
+
+    public int getOut() {
+        return out;
+    }
+
+    public void setOut(int out) {
+        this.out = out;
+    }
+
+    @Override
+    public String toString() {
+        return "Intersection{" +
+                "entry=" + entry +
+                ", bearings=" + bearings +
+                ", location=" + location +
+                ", in=" + in +
+                ", out=" + out +
+                '}';
+    }
+}
